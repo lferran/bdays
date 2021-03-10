bb28998efe7c44acbaf01034104f7b365dabd480
Ferran Llamas <ferran@onna.com>
2021-03-10 22:17:07 +0100

    add cors

--- src/backend/bdays/api.py
+++ src/backend/bdays/api.py
@@ -6,8 +6,18 @@ from pydantic import BaseModel, validator
 
 from bdays import db
 from bdays.models import Birthday
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class BirthdaySchema(BaseModel):
